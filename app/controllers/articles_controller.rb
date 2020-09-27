b886a2f889b6690dd5b6087900194048a4f69558
Nico Rithner <nicorithner@gmail.com>
2020-09-26 23:18:39 -0600

    Create update action for articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,9 +17,17 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
+
   def edit
     @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
   def destroy
     Article.find(params[:id]).destroy
     redirect_to articles_path
